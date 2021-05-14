242f5bed348101fd5767d2ae188f9bc025a5b88b
Ümit Dengiz <umitdengiz98@gmail.com>
2021-05-14 15:35:05 +0300

    JSON WEB TOKENS (JWT)

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,7 +1,9 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,5 +18,12 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError = "Kategorideki ürün sayısını aştınız!";
         public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün var!";
         internal static string CheckIfCategoryLimitExdeded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor!";
+        public static string AuthorizationDenied = "Yetkiniz yok!";
+        public static string PasswordError = "Parola hatası!";
+        public static string UserNotFound = "Kullanıcı bulunamadı!";
+        public static string UserRegistered = "Kayıt oldu.";
+        public static string SuccessfulLogin = "Başarılı giriş.";
+        public static string UserAlreadyExists = "Kullanıcı mevcut!";
+        public static string AccessTokenCreated = "Token oluşturuldu.";
     }
 }
